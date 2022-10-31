50ced8e0279d8a71d58eda9cae513307a861c336
Pekomon <5183385+pekomon@users.noreply.github.com>
2022-10-31 17:23:39 +0200

    Basic navigation between pages

---
+++ BasicNavigationDemo/app/src/main/java/com/example/pekomon/basicnavigationdemo/DetailScreen.kt
@@ -0,0 +1,46 @@
+package com.example.pekomon.basicnavigationdemo
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.pekomon.basicnavigationdemo.ui.theme.Purple80
+
+@Composable
+fun DetailScreen(
+    navController: NavController
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            modifier = Modifier.clickable {
+                // navController.popBackStack() would also work if no need for arguments
+                navController.navigate(Screen.Home.route) {
+                    popUpTo(Screen.Home.route) {
+                        inclusive = true
+                    }
+                }
+            },
+            text = "Detail",
+            color = Purple80,
+            fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun DetailScreenPreview() {
+    DetailScreen(rememberNavController())
+}
---
+++ BasicNavigationDemo/app/src/main/java/com/example/pekomon/basicnavigationdemo/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.pekomon.basicnavigationdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.pekomon.basicnavigationdemo.ui.theme.BasicNavigationDemoTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicNavigationDemoTheme {
+
+                navController = rememberNavController()
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SetupNavGraph(navController = navController)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BasicNavigationDemoTheme {
+        Greeting("Android")
+    }
+}
