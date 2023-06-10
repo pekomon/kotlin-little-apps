a6ac79954d0b075cb8a13c9569ac3f38348e94e7
Pekomon <5183385+pekomon@users.noreply.github.com>
2023-06-10 09:23:33 +0300

    Profile screen

---
+++ FirebaseLogin/app/src/main/java/com/example/pekomon/firebaselogin/presentation/profile/ProfileScreen.kt
@@ -0,0 +1,60 @@
+package com.example.pekomon.firebaselogin.presentation.profile
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.pekomon.firebaselogin.presentation.sign_in.UserData
+
+@Composable
+fun ProfileScreen(
+    userData: UserData,
+    onSignOut: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        userData.profilePictureUrl?.let {
+            AsyncImage(
+                model = userData.profilePictureUrl,
+                contentDescription = "Profile picture",
+                modifier = Modifier
+                    .size(160.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        userData.username?.let {
+            Text(
+                text = it,
+                textAlign = TextAlign.Center,
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        Button(
+            onClick = onSignOut,
+        ) {
+            Text(text = "Sign out")
+        }
+    }
+}
